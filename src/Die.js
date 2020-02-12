49f2279cd0f206292a24db5950454fae235bb030
GSingh1994 <singh.gagandeep0034@gmail.com>
2020-02-12 13:36:06 -0600

    Added state for second dice

--- src/Die.js
+++ src/Die.js
@@ -2,15 +2,12 @@ import React, { Component } from 'react';
 import './Die.css';
 
 class Die extends Component {
-
     render() {
         let fa = "fas fa-dice-"
         let { face } = this.props;
         return (
-            <div>
-                <div className='Die-dice'>
-                    <i className={fa + face}></i>
-                </div>
+            <div className='Die-dice'>
+                <i className={fa + face}></i>
             </div>
         );
     }
--- src/RollDice.js
+++ src/RollDice.js
@@ -2,22 +2,29 @@ import React, { Component } from 'react';
 import Die from './Die';
 import './RollDice.css'
 class RollDice extends Component {
+    static defaultProps = {
+        numArray: ['one', 'two', 'three', 'four', 'five', 'six']
+    };
     constructor(props) {
         super(props);
         this.state = {
-            num: 0
+            num1: 'six',
+            num2: 'six'
         };
-        this.randomNum = this.randomNum.bind(this);
+        // this.roll = this.roll.bind(this);
     }
-    randomNum() {
-        this.setState({ num: Math.floor(Math.random() * 6) })
+    roll = () => {
+        this.setState({
+            num1: this.props.numArray[Math.floor(Math.random() * this.props.numArray.length)],
+            num2: this.props.numArray[Math.floor(Math.random() * this.props.numArray.length)]
+        })
     };
     render() {
-        let numArray = ['one', 'two', 'three', 'four', 'five', 'six'];
         return (
             <div>
-                <Die face={numArray[this.state.num]} />
-                <button onClick={this.randomNum} className='RollDice-btn'>Roll Dice!</button>
+                <Die face={this.state.num1} />
+                <Die face={this.state.num2} />
+                <button onClick={this.roll} className='RollDice-btn'>Roll Dice!</button>
             </div>
         );
     }
