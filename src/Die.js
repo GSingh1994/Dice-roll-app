31c0ec1f608aefb64845e86b53b9cd6e83f7f08e
GSingh1994 <singh.gagandeep0034@gmail.com>
2020-02-12 11:48:18 -0600

    Added fontAwesome and dice face changes onClick

--- src/Die.js
+++ src/Die.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
+import './Die.css';
+
 class Die extends Component {
+
     render() {
+        let fa = "fas fa-dice-"
+        let { face } = this.props;
         return (
             <div>
-                Hello from Die
+                <div className='Die-dice'>
+                    <i className={fa + face}></i>
+                </div>
             </div>
         );
     }
--- src/RollDice.js
+++ src/RollDice.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
 import Die from './Die';
+import './RollDice.css'
 class RollDice extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            num: 0
+        };
+        this.randomNum = this.randomNum.bind(this);
     }
+    randomNum() {
+        this.setState({ num: Math.floor(Math.random() * 6) })
+    };
     render() {
+        let numArray = ['one', 'two', 'three', 'four', 'five', 'six'];
         return (
             <div>
-                <Die />
+                <Die face={numArray[this.state.num]} />
+                <button onClick={this.randomNum} className='RollDice-btn'>Roll Dice!</button>
             </div>
         );
     }
